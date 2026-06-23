f97dcb31332a8742cd865eac34e7322d31df58d5
Adam Ahmed <sdi2400225@di.uoa.gr>
2026-06-24 00:29:37 +0300

    Add transmission pool

--- k29photo/db.py
+++ k29photo/db.py
@@ -3,12 +3,11 @@ import psycopg2
 import psycopg2.extras
 from flask import g
 
-
 def get_db():
     if 'db' not in g:
         database_url = os.environ.get('DATABASE_URL')
         if database_url:
-            g.db = psycopg2.connect(database_url, sslmode='require')
+            g.db = psycopg2.connect(database_url)
         else:
             g.db = psycopg2.connect(
                 dbname='k29photo',
@@ -18,18 +17,14 @@ def get_db():
         g.db.autocommit = False
     return g.db
 
-
 def get_cursor():
     return get_db().cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
 
 def commit():
     get_db().commit()
 
-
 def rollback():
     get_db().rollback()
-
 
 def close_db(exception=None):
     db = g.pop('db', None)
